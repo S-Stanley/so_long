ada39d033b1c82e7a0a99ea9cd763b0de00d96d6
Stanley SERBIN <sserbin@e2r11p9.clusters.42paris.fr>
2021-12-05 14:49:28 +0100

    change keycode linux qwerty ok

--- sources/hook.c
+++ sources/hook.c
@@ -27,14 +27,15 @@ int	on_key_press(int keycode, t_window *window)
 {
 	t_player	player;
 
+	printf("%d\n", keycode);
 	player = get_player_position(window->map);
 	if (keycode == 65307)
 		exit_game(*(window));
-	if (keycode == 65362 || keycode == 97)
+	if (keycode == 65362 || keycode == 119)
 		*(window) = move_top(window->map, player, *(window));
-	if (keycode == 65364 || keycode == 119)
+	if (keycode == 65364 || keycode == 115)
 		*(window) = move_bottom(window->map, player, *(window));
-	if (keycode == 65361 || keycode == 115)
+	if (keycode == 65361 || keycode == 97)
 		*(window) = move_left(window->map, player, *(window));
 	if (keycode == 65363 || keycode == 100)
 		*(window) = move_right(window->map, player, *(window));
